ff912b331347698925ac2a8321c469e59c3b6998
xPhelippe <souzaherodp@gmail.com>
2021-06-03 21:03:41 -0400

    Add toggleButton

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -22,6 +22,8 @@ private:
     //==============================================================================
     // Your private member variables go here...
 
+    juce::ToggleButton toggleButton;
+
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
